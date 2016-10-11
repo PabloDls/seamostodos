551053dc513d20964986e080341e00aff306fa1f
rafaloz7 <rafaloz7@gmail.com>
2016-10-11 09:33:52 -0500

    clase eventos

--- db/migrate/20160929013649_drop_table_events.rb
+++
@@ -1,6 +0,0 @@
-class DropTableEvents < ActiveRecord::Migration
-  def change
-
-  	drop_table :event
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -37,10 +37,4 @@ ActiveRecord::Schema.define(version: 20160929024034) do
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
 
-  create_table "events", force: :cascade do |t|
-    t.string   "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
